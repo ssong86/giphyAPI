078d41443bf0bca4622fe416b18c0dbf9f2fafa6
Sangwon Song <ssong86@gatech.edu>
2017-09-23 18:32:42 -0400

    Have to rebuild the logic

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,6 +1,6 @@
-var topics = ['Sushi', 'Pizza', 'Pasta', 'Fried Chicken'];
+var topics = ['France', 'Mexico', 'Korea', 'Japan', 'China', 'Italy'];
 var currentGif;
-var pauseeGif;
+var pausedGif;
 var animatedGif;
 var stillGif;
 
@@ -8,28 +8,59 @@ var stillGif;
 function createButtons(){
 	// a fucntion displaying buttons to show
 	$("#buttons").empty();
-	for(var i = 0; i < topcis.length; i++){
+	for(var i = 0; i < topics.length; i++){
 		//write the button names imported from giphy api
-		var showBtn = $("<button>").text(topics[i]).addClass("showBtn").attr("data-name": topcis[i]);
+		var showBtn = $("<button>").text(topics[i]).addClass("showBtn").attr({"data-name": topics[i]});
 		$("#buttons").append(showBtn);
 	}
 
 	// on click function for gifs
-	$(".showBtn").co("click", function(){
+	$(".showBtn").on("click", function(){
 		$(".display").empty();
 
-		//var thisShow = $(this).data("name");
-		var giphyURL = "https://api.giphy.com/v1/stickers/search?api_key=1ZJH8kOA2RGcK21tB4rw7JkDrSfHcR1N&q=food&limit=10&offset=0&rating=G&lang=en";
+		var thisShow = $(this).data("name");
+		var giphyURL = "https://api.giphy.com/v1/gifs/search?api_key=1ZJH8kOA2RGcK21tB4rw7JkDrSfHcR1N&q="+ thisShow+ "&limit=10&offset=0&rating=G&lang=en";
 		$.ajax({url:giphyURL, method: 'GET'}).done(function(giphy){
 			currentGif = giphy.data;
 			$.each(currentGif, function(index,value){
 				animatedGif = value.images.original.url;
 				pausedGif = value.images.original_still.url;
 				var thisRating = value.rating;
-				//gives blank ratings 	
+				//gives blank ratings 'unrated' text
+				if(thisRating === ''){
+					thisRating = 'unrated';
+				}
+				var rating = $('<h5>').html("Rated: "+thisRating).addClass('ratingStyle');
+				stillGif = $('<img>').attr('data-animated', animatedGif).attr('data-paused', pausedGif).attr('src', pausedGif).addClass('gif');
+				var fullGifDisplay = $('<button>').append(rating, stillGif);
+				$(".display").append(fullGifDisplay);
 			});
 		});
-
-
 	});
 }
+
+//animates and pause gif on hover
+$(".gif").on("click", function(){
+	var state = $(this).attr("data-attr");
+	
+});
+$(document).on("click", ".playOnHover", function(){
+
+	$(this).attr('src', $(this).data('animated'));
+});
+
+$(document).on("click", ".playonHover", function(){
+	$(this).attr('src', $(this).data('paused'));
+});
+
+//sets a button from input
+$('#addShow').on('click', function(){
+	var newShow = $('#newShowInput').val().trim();
+	topics.push(newShow);
+	createButtons();
+	return false;
+});
+
+createButtons();
+
+
